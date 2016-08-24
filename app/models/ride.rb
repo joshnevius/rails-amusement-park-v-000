4cec4422001358842fd99685c5fe6fa6aab06c5e
joshnevius <joshnevius92@gmail.com>
2016-08-24 17:56:41 -0400

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,36 @@
 class Ride < ActiveRecord::Base
-  # write associations here
+  belongs_to :user
+  belongs_to :attraction
+
+  def enough_tickets?
+    if user.tickets < attraction.tickets
+      "Sorry. You do not have enough tickets for the #{attraction.name}."
+    end
+  end
+ 
+  def tall_enough?
+    if self.user.height < self.attraction.min_height
+      "Sorry. You are not tall enough to ride the #{attraction.name}."
+    end
+  end
+
+  def take_ride
+    if enough_tickets? && tall_enough?
+      "Sorry. You do not have enough tickets for the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+    elsif enough_tickets?
+      enough_tickets?
+    elsif tall_enough?
+      tall_enough?
+    else
+      user = self.user
+      new_happiness = user.happiness + self.attraction.happiness_rating
+      new_nausea = user.nausea + self.attraction.nausea_rating
+      new_tickets =  user.tickets - self.attraction.tickets 
+      user.update!(happiness: new_happiness)
+      user.update!(nausea: new_nausea)
+      user.update!(tickets: new_tickets)
+      user.save
+      "Thanks for riding the #{self.attraction.name}!"
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ActiveRecord::Base
-  # write associations here
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  has_secure_password
+
+  def mood
+    if self.nausea.nil? || self.happiness.nil?
+      return "No mood"
+    elsif
+      self.nausea > self.happiness
+      return "sad"
+    else
+      return "happy"
+    end
+  end
 end
